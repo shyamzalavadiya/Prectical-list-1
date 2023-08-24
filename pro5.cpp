ca4727a891fb47b5db0b673f27f2ecc8aef34313
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:26:19 +0530

    pro5.cpp

---
+++ pro5.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a=5,b=6;
+	int temp;
+
+	cout<<"220130318015"<<endl;
+	cout<<"shyam zalavadiya"<<endl;
+
+	cout<<"Enter the number::";
+
+	cout<<"a::"<<a<<endl<<"b::"<<b<<endl;
+
+
+	temp=a;
+	a=b;
+	b=temp;
+
+	cout<<"swap number is"<<endl;
+
+	cout<<"a::"<<a<<endl<<"b::"<<b<<endl;
+
+	getch();
+}
