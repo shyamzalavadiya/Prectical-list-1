b632807a8931346dd47c64a32518a6475eab1a58
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:27:15 +0530

    Create pro6.cpp

---
+++ pro6.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int p,r,t;
+	clrscr();
+
+	cout<<"220130318015"<<endl;
+	cout<<"shyam zalavadiya"<<endl;
+
+	cout<<"Enter the principal Amount::";
+	cin>>p;
+
+	cout<<"Enter the rate::";
+	cin>>r;
+
+	cout<<"Enter the time::";
+	cin>>t;
+
+	cout<<"Simple intrest is::"<<p*r*t/100;
+
+	getch();
+}
