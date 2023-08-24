deec46aa2dec7fecd9814f860e2eda86f0001872
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:28:09 +0530

    pro7.cpp

---
+++ pro7.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int p,r,t;
+	clrscr();
+
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+
+	cout<<"Enter the principal Amount::";
+	cin>>p;
+
+	cout<<"Enter the rate::";
+	cin>>r;
+
+
+	cout<<"Enter the time::";
+	cin>>t;
+
+	cout<<"Simple intrest is::"<<p*r*t/100;
+
+	getch();
+}
