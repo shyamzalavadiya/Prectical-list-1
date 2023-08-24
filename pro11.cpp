535dedcd79b005382e27ff922e0e981dc04279ca
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:34:24 +0530

    pro11.cpp

---
+++ pro11.cpp
@@ -0,0 +1,50 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	char str;
+	clrscr();
+
+	cout<<"220130318015"<<endl;
+	cout<<"shyam zalavadiya"<<endl;
+
+	cout<<"Enter the character::";
+	cin>>str;
+
+	switch(str)
+	{
+		case 'A':
+		cout<<"This is vowel";
+		break;
+		case 'E':
+		cout<<"This is vowel";
+		break;
+		case 'I':
+		cout<<"This is vowel";
+		break;
+		case 'O':
+		cout<<"This is vowel";
+		break;
+		case 'U':
+		cout<<"This is vowel";
+		break;
+		case 'a':
+		cout<<"This is vowel";
+		break;
+		case 'e':
+		cout<<"This is vowel";
+		break;
+		case 'i':
+		cout<<"This is vowel";
+		break;
+		case 'o':
+		cout<<"This is vowel";
+		break;
+		case 'u':
+		cout<<"This is vowel";
+		break;
+		default:
+		cout<<"This is constant";
+	}
+	getch();
+}
