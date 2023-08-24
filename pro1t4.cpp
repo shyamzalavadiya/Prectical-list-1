715c933e5810d377c48706f4c4aa2d5290e867b2
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:20:38 +0530

    pro1t4.cpp

---
+++ pro1t4.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+	int redius;
+	float pi=3.14;
+	clrscr();
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+	cout<<"Enter the redius:)";
+	cin>>redius;
+	cout<<"area is:)"<<pi*redius*redius<<endl;
+	cout<<"circumfarence is:)"<<2*pi*redius<<endl;
+	cout<<"Diameter is:)"<<2*redius<<endl;
+	getch();
+}
