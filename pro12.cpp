d3dc91f8729ebf482a61a9fc1ef84c2b414831c7
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:35:00 +0530

    pro12.cpp

---
+++ pro12.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int a,i;
+	clrscr();
+
+	cout<<"220130318007"<<endl;
+	cout<<"sani zalavadiya"<<endl;
+
+	cout<<"Enter the table number::";
+	cin>>a;
+
+	for(i=1;i<11;i++)
+	{
+		cout<<a<<"*"<<i<<"="<<a*i<<endl;
+	}
+	getch();
+}
