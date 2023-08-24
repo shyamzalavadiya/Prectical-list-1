2b5ddb310ad5753e2165062d535293bd156ba5ea
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:30:35 +0530

    pro9.cpp

---
+++ pro9.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	cout<<"220130318015"<<endl;
+	cout<<"shyam zalavadiya"<<endl;
+
+	cout<<"Enter the number::";
+	cin>>n;
+
+	if(n%2==0)
+	{
+		cout<<"This is Even Number";
+	}
+	else
+	{
+		cout<<"This is Odd Number";
+	}
+	getch();
+}
