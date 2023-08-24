1028f9a8643c2f7ed73a8c680ee3fc222aa8d975
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:29:21 +0530

    pro8.cpp

---
+++ pro8.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int p,r,t;
+	clrscr();
+
+	cout<<"220130318015"<<endl;
+	cout<<"shyam zalavadiya"<<endl;
+
+	cout<<"Enter the amount::";
+	cin>>p;
+
+	cout<<"Enter the rate::";
+	cin>>r;
+
+	cout<<"Enter the time::";
+	cin>>t;
+
+	cout<<p*((1+r/100),t);
+
+	getch();
+}
