f9903dc78df0742e62c6617f3f7e8f5887587945
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:19:54 +0530

    pro1t2.cpp

---
+++ pro1t2.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	cout<<"220130318015";
+	cout<<"shyam zalavadiya";
+	cout<<"Enter the number:)";
+	cin>>a;
+	cout<<"Enter the number:)";
+	cin>>b;
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	cout<<a/b<<endl;
+	cout<<a%b<<endl;
+	getch();
+}
