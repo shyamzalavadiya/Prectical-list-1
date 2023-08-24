7ebbd4a379c6712d30bbe58f377dc468745d20d0
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-08-24 08:21:24 +0530

    pro1t5.cpp

---
+++ pro1t5.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n,i;
+	int a[5];
+	clrscr();
+
+
+	cout<<"220130318007"<<endl;
+	cout<<"Vasu Sapariya"<<end;
+
+	cout<<"Enter the size of array::";
+	cin>>a[i];
+
+	cout<<"Enter elements in array::";
+
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+		if(a[i]<0)
+		{
+		cout<<"a[%d]::%d"<<i<<a[i]<<endl;
+		}
+	}
+	getch();
+}
